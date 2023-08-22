f72c98b68e03d41a1b979e18a7fae5c7da31db41
Priyanka Singh Tomer <136883301+PST09@users.noreply.github.com>
2023-08-22 14:31:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function decimalToBinary(num) {
-  //Write you code here
+ arr.sort();
+	let n=arr.length;
+	for(let i=0;i<n-1;i++){
+		const vat = Math.abs(arr[0]-arr[1]);
+		if(vat==1){
+			arr.shift();
+		}
+	}
+	if(arr.length==1){
+		return "YES";
+	}
+	else{
+		return "NO";//Write you code here
   
 }
 
